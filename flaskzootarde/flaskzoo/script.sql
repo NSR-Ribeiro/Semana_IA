7573f68cf1ed9d716914baf74cd332d02400317e
NSR-Ribeiro <145523740+NSR-Ribeiro@users.noreply.github.com>
2024-10-03 17:50:01 -0300

    Add files via upload

---
+++ flaskzootarde/flaskzoo/script.sql
@@ -0,0 +1,8 @@
+create database zooflasktarde;
+use zooflasktarde;
+create table animal(
+  id int primary key auto_increment,
+  nome_popular varchar(80) NOT NULL,
+  nome_cientifico varchar(60) NOT NULL,
+  habitos_noturnos LONGTEXT NOT NULL
+)
